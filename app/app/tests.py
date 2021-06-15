2c707dd4665ebc2198f7ef17b876bd05317fb504
jcamier <jcamier@gmail.com>
2021-06-15 08:04:41 -0500

    add basic unittest

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from .calc import add
+
+# to run tests
+# docker-compose run app sh -c "python manage.py test"
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3, 8), 11)
