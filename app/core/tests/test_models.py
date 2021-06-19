5b10feb5e98bf9b98e4eeb0f79496fd541eec9b1
jcamier <jcamier@gmail.com>
2021-06-19 12:17:44 -0500

    add admin tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,7 +15,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
+        email = 'test@gmail.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -27,7 +27,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@LONDONAPPDEV.COM'
+        email = 'test@GMAIL.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
@@ -40,7 +40,7 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@londonappdev.com',
+            'test@gmail.com',
             'test123'
         )
 
